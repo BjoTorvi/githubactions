7549271dd36515f965211c5cf546e5dbc6bf6047
BjoTorvi <47891910+BjoTorvi@users.noreply.github.com>
2023-03-12 15:07:27 +0100

    Update Program.cs

--- TestGithubActions/TestGithubActions/Program.cs
+++ TestGithubActions/TestGithubActions/Program.cs
@@ -7,6 +7,8 @@ namespace TestGithubActions
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(($"Dont tell anybody, but {Environment.GetEnvironmentVariable("env_secret")}");
+);
         }
     }
 }
